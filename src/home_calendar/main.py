e7b3283bfbfc6afe8de3256062b3a5b881135fa2
oldrich.kodym@gmail.com <oldrich.kodym@gmail.com>
2025-01-01 20:55:55 +0100

    add exception handling in main loop

--- src/home_calendar/main.py
+++ src/home_calendar/main.py
@@ -18,23 +18,29 @@ if __name__ == "__main__":
     epd.Clear()
     
     while True:
-        print('pulling cal...')
-        cal_parser.update_calendar()
-        print('rendering..')
-        page = CalendarPage(num_days=7)
+        try:
+            print('pulling cal...')
+            cal_parser.update_calendar()
+            print('rendering..')
+            page = CalendarPage(num_days=7)
         
-        for i in range(page.num_days):    
-            events = cal_parser.get_days_events(datetime.date.today() + datetime.timedelta(days=i))
-            for event in events:
-                page.render_event(event, day=i)
+            for i in range(page.num_days):    
+                events = cal_parser.get_days_events(datetime.date.today() + datetime.timedelta(days=i))
+                for event in events:
+                    page.render_event(event, day=i)
         
-        page.render_busy_status(cal_parser.get_busy_status())
+            page.render_busy_status(cal_parser.get_busy_status())
         
-        print('displaying')
-        epd.init_fast()
-        epd.display(epd.getbuffer(page.get_image()))
+            print('displaying')
+            epd.init_fast()
+            epd.display(epd.getbuffer(page.get_image()))
         # page.show()
         
-        print('done, sleeping for 10 min')
-        epd.sleep()
+            print('done, sleeping for 10 min')
+            epd.sleep()
+        
+        except Exception as e:
+            print("failed refreshing due to exception:")
+            print(e)
+
         time.sleep(10 * 60)  # sleep 10 minutes
